71832265a6e0790de7e73cde7390e63af4c9d023
Tejasvi Vishwanadha <tvishwanadha@gmail.com>
2014-05-18 11:43:42 -0700

    refactor auth + basic chat starting

--- app/js/component/dataAuth.js
+++ app/js/component/dataAuth.js
@@ -34,43 +34,94 @@ define(function (require) {
     function dataAuth() {
         var self = this;
 
-        this.fetchUser = function() {
+        this.getAccessToken = function() {
             var access_token = querystring('access_token');
+            if (!access_token) {
+                access_token = window.localStorage.getItem('access_token');
+            }
             if (!access_token) {
                 var LOGIN_URL = window.location.host + '/login.html'
                 LOGIN_URL += window.storage.genreId ?  "#" + window.storage.genreId : "";
                 return window.location.href = LOGIN_URL;
             }
+            else {
+                window.localStorage['access_token'] = access_token;
+            }
+
+            return access_token;
+        };
+
+        this.fetchUser = function(evt, msg) {
+
+            var access_token = this.getAccessToken();
+
+            if (!access_token) return;
 
             $.ajax({
                 url: "https://partner.api.beatsmusic.com/v1/api/me",
                 crossDomain: true,
                 dataType: 'json',
-                beforeSend: function (xhr) {                    
+                beforeSend: function (xhr) {
                     xhr.setRequestHeader("Authorization", "Bearer " + access_token);
                     xhr.setRequestHeader("Access-Control-Allow-Origin", "*")
                 },
                 success: function(data, status) {
                     if (data && data.result) {
                         window.localStorage["user"] = JSON.stringify(data.result);
-                        var user_id = data.result.user_context;
-                        var user = {};
-                        user[user_id] = {
-                            vote: 0,
-                            data: data.result,
-                            //username: data.result.username, // TODO: fetch separately, does not come from /me
-                        };
-                        window.users.push(user);
+                        self.fetchUserData();
                     }
                 },
                 failure: function(data, status) {
                     console.error(status, data);
                 }
-            })
+            });
+        };
+
+        this.fetchUserData = function(evt, msg) {
+            var access_token = this.getAccessToken();
+
+            if (!access_token) return;
+
+            var user = JSON.parse(window.localStorage["user"]);
+
+            if (!user) return;
+
+            $.ajax({
+                url: "https://partner.api.beatsmusic.com/v1/api/users/" + user.user_context,
+                crossDomain: true,
+                dataType: 'json',
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("Authorization", "Bearer " + access_token);
+                    xhr.setRequestHeader("Access-Control-Allow-Origin", "*")
+                },
+                success: function(data, status) {
+                    if (data && data.data) {
+                        window.localStorage["user"]['data'] = JSON.stringify(data.data);
+                        var user_id = data.data.id;
+                        var user = {};
+                        user[user_id] = {
+                            vote: 0,
+                            data: data.data,
+                            id : user_id,
+                            username: data.data.username
+                        };
+                        window.users.update(user);
+                        self.broadCast(data);
+                    }
+                },
+                failure: function(data, status) {
+                    console.error(status, data);
+                }
+            });
+        };
+
+        this.broadCast = function(data) {
+            this.trigger(document, 'dataUserData', { user : data });
         };
 
         this.after('initialize', function () {
-            self.fetchUser();
+            this.on('uiNeedsAuth', this.fetchUser);
+            this.on('uiNeedsUserData', this.fetchUserData);
         });
     }
 });
