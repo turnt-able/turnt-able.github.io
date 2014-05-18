96523972055642d106ecd7c6fdb816c3b00d872f
Justin Higgins <justinhiggy@gmail.com>
2014-05-18 10:48:48 -0700

    gh-pages workaround

---
+++ js/component/uiUser.js
@@ -0,0 +1,66 @@
+define(function (require) {
+
+  'use strict';
+
+  /**
+   * Module dependencies
+   */
+
+  var defineComponent = require('flight/lib/component');
+  var withHogan = require('flight-hogan/lib/with_hogan');
+
+  /**
+   * Module exports
+   */
+
+  return defineComponent(withHogan, user);
+
+  /**
+   * Module function
+   */
+
+  function user() {
+    var users = {};
+
+    this.defaultAttrs({
+      'template': '<div class="user" id={{id}} style="background-image:url(/img/avatars/1_back_rock_h.png)"></div>'
+    });
+
+    this.$forUser = function (id) {
+      return $('#' + id);
+    };
+
+    this.addUser = function (evt, msg) {
+      this.$node.append(this.renderTemplate(this.attr.template, msg.user));
+    };
+
+    this.removeUser = function (evt, msg) {
+      delete users[msg.user.id];
+      this.$forUser(msg.user.id).remove();
+    };
+
+    this.updateUser = function (evt, msg) {
+      var user = msg.user;
+      if (user.rated !== users[user.id].rated) {
+        this.showUserRating(user);
+      }
+    };
+
+    this.showUserRating = function (user) {
+      var $user = this.$forUser(user.id).find('.user');
+      if (user.vote === 1) {
+        $user.addClass('bounce')  
+      } else {
+        $user.removeClass('bounce');
+      }
+      
+    };
+
+    this.after('initialize', function () {
+      this.on(document, 'dataUserJoined',  this.addUser);
+      this.on(document, 'dataUserLeft',    this.removeUser);
+      this.on(document, 'dataUserChanged', this.updateUser);
+    });
+  }
+
+});
---
+++ js/page/default.js
@@ -0,0 +1,53 @@
+define(function (require) {
+
+  'use strict';
+
+  /**
+   * Module dependencies
+   */
+  var DataState = require('component/dataState');
+  var PlaylistMaker = require('component/playlistMaker');
+  var UIDj = require('component/uiDj');
+  var UIUser = require('component/uiUser');
+  var UIRating = require('component/uiRating');
+  var UIMarquee = require('component/uiMarquee');
+  var DataBAM = require('component/dataBAM');
+  var DataAuth = require('component/dataAuth');
+
+  /**
+   * Module exports
+   */
+
+  return initialize;
+
+  /**
+   * Module function
+   */
+
+  function initialize() {
+    DataBAM.attachTo(document);
+    UIDj.attachTo('#djs');
+    UIUser.attachTo('#users');
+    UIRating.attachTo('#rate-box');
+    UIMarquee.attachTo(document);
+    DataState.attachTo(document,  {
+      fireBaseUrl: 'https://uralgosux.firebaseio.com/'
+    });
+
+    var user = window.localStorage.getItem("user");
+    if (!user) {
+        var DataAuth = require('component/dataAuth');
+        DataAuth.attachTo(document);
+    }
+    PlaylistMaker.attachTo(document);
+
+    // // test playlist
+    // $(document).on('dataPlaylistTracks', function(event, msg) {
+    //   // $('body').html('<pre>' + JSON.stringify(msg, null, '\t') + '</pre>');
+    //   console.dir(msg);
+    // });
+    // $(document).trigger('uiNeedsPlaylist', { artist_name : 'Weezer' });
+
+  }
+
+});
